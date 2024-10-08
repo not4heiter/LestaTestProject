e605a04072bb0c47d0e2bf8baa3f4ffa09a7019d
not4heiter <dead-man.heiter@inbox.ru>
2024-10-08 20:36:48 +0300

    Second Commit

--- Assets/Scripts/JumpBlock.cs
+++ Assets/Scripts/JumpBlock.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class JumpBlock : MonoBehaviour
 {
-    public float knockbackAngle = 45f;
     public float knockbackForce = 5f;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.CompareTag("Player"))
         {
             CharacterController playerController = other.GetComponent<CharacterController>();
             if (playerController != null)
             {
-                Vector3 knockbackDirection = Quaternion.Euler(0, 0, knockbackAngle) * Vector3.back;
+                Vector3 velocity = playerController.velocity;
+
+                if (velocity.magnitude < 0.1f)
+                {
+                    velocity = other.transform.forward;
+                }
+
+                velocity.Normalize();
+
+                Vector3 knockbackDirection = -velocity;
                 knockbackDirection.y = 0;
                 knockbackDirection.Normalize();
 
                 playerController.Move(knockbackDirection * knockbackForce * Time.deltaTime);
-
-                Vector3 jump = Vector3.up * Time.deltaTime * (knockbackForce / 2);
-                playerController.Move(jump);
             }
         }
     }
